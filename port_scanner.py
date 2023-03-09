07831c1f0f3fbb4eac547d29863b4920c39c8cc4
SWTD 18E <pseude18@gmail.com>
2023-03-09 12:20:22 -0700

    lmks

--- port_scanner.py
+++ port_scanner.py
@@ -1,10 +1,17 @@
 import socket
 import nmap
+from common_ports import ports_and_services
 
 scanner=nmap.PortScanner()
 
-def get_open_ports(target, port_range):
+def get_open_ports(target, port_range,verbose=True):
     scanner.scan(target,''+str(port_range[0])+'-'+ str(port_range[1])+'','-v -sS')
     open_ports=scanner[target]['tcp'].keys()
-    return(open_ports)
+    if(verbose):
+        text='the  open ports for {'+target+'}are:\nPort\tService\n'
+        for port in open_ports :
+            text=text+str(port)+'\t'+str(ports_and_services.get(port))+'\n'
+    return text
+    else:
+        return open_ports
 
