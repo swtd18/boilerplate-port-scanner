87988660ddf82359ea2552e2695331373d98e223
SWTD 18E <pseude18@gmail.com>
2023-03-09 06:20:38 -0700

    qddqdd

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,9 @@
 import socket
+import nmap
+
+scanner=nmap.PortScanner()
 
 def get_open_ports(target, port_range):
-    open_ports = []
-
-
-
+    scanner.scan(target,"'"+port_range[0]+port_range[1]+"'",'-v -sS')
+    open_ports = scanner[target]['tcp'].keys()
     return(open_ports)
